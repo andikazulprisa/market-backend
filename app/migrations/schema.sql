2fbbbdaabcf272162e93b1b9b9e70c50cc45fdaa
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-04-19 08:26:46 +0700

    update schema

--- app/migrations/schema.sql
+++ app/migrations/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY SERIAL,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 
 CREATE TABLE products (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY SERIAL,
     name VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
     price FLOAT NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE products (
 );
 
 CREATE TABLE transactions (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY SERIAL,
     user_id INT,
     product_id INT,
     quantity INT NOT NULL,
